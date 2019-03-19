98243e5a84eb84c34518e0b8a368aec9ee74f04b
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2019-03-18 20:56:13 -0700

    Train model

--- model.py
+++ model.py
@@ -22,41 +22,30 @@ class EncoderCNN(nn.Module):
 
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1, batch_size=10, drop_prob=0.5):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1, drop_prob=0.5):
         ''' Initialize the layers of this model.'''
         super(DecoderRNN, self).__init__()
 
-        self.batch_size = batch_size
-        self.hidden_size = hidden_size
-
+        # the Embedding layer for captions
         self.embed = nn.Embedding(vocab_size, embed_size)
-
-        # initialize the hidden state (see code below)
-        self.hidden = self.init_hidden()
 
         # the LSTM takes embedded word vectors (of a specified size) as inputs
         # and outputs hidden states of size hidden_size
-        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers=num_layers, dropout=drop_prob, batch_first=True)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers=num_layers, batch_first=True)
+
+        # the Drop out layer between the LSTM layer and the fully connected layer
+        self.dropout = nn.Dropout(drop_prob)
 
         # the linear layer that maps the hidden state output dimension
         # to the number of tags we want as output, tagset_size (in this case this is 3 tags)
         self.hidden2vocab = nn.Linear(hidden_size, vocab_size)
-
-    def init_hidden(self):
-        ''' At the start of training, we need to initialize a hidden state;
-           there will be none because the hidden state is formed based on perviously seen data.
-           So, this function defines a hidden state with all zeroes and of a specified size.'''
-        # The axes dimensions are (n_layers, batch_size, hidden_size)
-        hidden = (torch.zeros(1, self.batch_size, self.hidden_size),
-                  torch.zeros(1, self.batch_size, self.hidden_size))
-        return hidden
 
     def forward(self, features, captions):
         # Arrange inputs
         features = features.unsqueeze(1)
         embeddings = self.embed(captions[:, :-1])
         lstm_inputs = torch.cat((features, embeddings), 1)
-        lstm_out, self.hidden = self.lstm(lstm_inputs, self.hidden)
+        lstm_out, _ = self.lstm(lstm_inputs)
         vocab_outputs = self.hidden2vocab(lstm_out)
         return vocab_outputs
 
